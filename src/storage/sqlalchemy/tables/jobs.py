e5843c0b2878c67213e763cb7634f1cf93ad2cd4
fuji1o <fuji.o@yandex.ru>
2025-05-19 00:12:06 +0500

    feat(job): добавлена таблица Jobs

--- src/storage/sqlalchemy/tables/jobs.py
+++ src/storage/sqlalchemy/tables/jobs.py
@@ -1,5 +1,8 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String, Boolean, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from decimal import Decimal
+from datetime import datetime
+
 
 from storage.sqlalchemy.client import Base
 
@@ -11,8 +14,13 @@ class Job(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"), comment="Идентификатор пользователя"
     )
-
-    # добавьте ваши колонки сюда
+    title: Mapped[str] = mapped_column(String(100), comment="Название вакансии")
+    description: Mapped[str] = mapped_column(Text, comment="Описание вакансии")
+    salary_from: Mapped[Decimal] = mapped_column(nullable=True, comment="Минимальная зарплата")
+    salary_to: Mapped[Decimal] = mapped_column(nullable=True, comment="Максимальная зарплата")
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, comment="Актуальность вакансии")
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, comment="Дата создания")
 
     user: Mapped["User"] = relationship(back_populates="jobs")  # noqa
     responses: Mapped["Response"] = relationship(back_populates="job")  # noqa
+    
