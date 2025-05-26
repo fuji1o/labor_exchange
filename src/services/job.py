13e928d347d86b3c605e8cc9330bb116476ec229
fuji1o <fuji.o@yandex.ru>
2025-05-26 23:06:07 +0500

    refactor(job): переписан jobservice с исп-ем baseservice

--- src/services/job.py
+++ src/services/job.py
@@ -1,40 +1,39 @@
 from interfaces.i_repository import IRepositoryAsync
+from services.base_service import BaseService
 from services.exseptions import JobNotFoundError
 from web.schemas.job import JobCreateSchema, JobUpdateSchema
 
 
-class JobService:
+class JobService(BaseService):
     def __init__(self, job_repository: IRepositoryAsync):
-        self.job_repository = job_repository
+        super().__init__(job_repository)
 
     async def create(self, is_company: bool, job_create_dto: JobCreateSchema):
         if not is_company:
             raise PermissionError("Недостаточно прав")
-        return await self.job_repository.create(job_create_dto=job_create_dto)
+        return await self.repository.create(job_create_dto=job_create_dto)
 
     async def retrieve(self, **kwargs):
-        try:
-            return await self.job_repository.retrieve(**kwargs)
-        except:
+        job = await super().retrieve(**kwargs)
+        if not job:
             raise JobNotFoundError("Вакансия не найдена")
+        return job
 
     async def retrieve_many(self, limit: int, skip: int):
-        return await self.job_repository.retrieve_many(limit=limit, skip=skip)
+        return await super().retrieve_many(limit=limit, skip=skip)
 
     async def update(self, id: int, job_update_dto: JobUpdateSchema, user_id: int):
-        job = await self.job_repository.retrieve(id=id)
+        job = await self.repository.retrieve(id=id)
         if not job:
             raise JobNotFoundError("Вакансия не найдена")
         if job.user.id != user_id:
             raise PermissionError("Недостаточно прав")
-        return await self.job_repository.update(id=id, job_update_dto=job_update_dto)
+        return await self.repository.update(id=id, job_update_dto=job_update_dto)
 
     async def delete(self, id: int, user_id: int):
-        job = await self.job_repository.retrieve(id=id)
-        if not job:
-            raise JobNotFoundError("Вакансия не найдена")
-
-        if job.user.id != user_id:
-            raise PermissionError("Недостаточно прав")
-
-        return await self.job_repository.delete(id=id)
+        return await self.delete_with_permission_check(
+            id=id,
+            user_id=user_id,
+            get_owner_id=lambda job: job.user.id,
+            not_found_exc=JobNotFoundError("Вакансия не найдена"),
+        )
