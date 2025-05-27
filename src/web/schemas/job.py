2537b38ab89e5725fddcbe50a99da71dbe392c35
fuji1o <fuji.o@yandex.ru>
2025-05-27 09:58:38 +0500

    refactor(job): внесены изменения в контракт job

--- src/web/schemas/job.py
+++ src/web/schemas/job.py
@@ -2,17 +2,16 @@ from datetime import datetime
 from decimal import Decimal
 from typing import Optional
 
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, Field, model_validator
 from typing_extensions import Self
 
 
 class JobCreateSchema(BaseModel):
-    title: str
-    description: str
-    salary_from: Optional[Decimal] = None
-    salary_to: Optional[Decimal] = None
-    is_active: Optional[bool] = True
-    created_at: Optional[datetime] = None
+    title: str = Field(..., description="Название вакансии")
+    description: str = Field(..., description="Описание вакансии")
+    salary_from: Optional[Decimal] = Field(default=None, description="Минимальная зарплата")
+    salary_to: Optional[Decimal] = Field(default=None, description="Максимальная зарплата")
+    is_active: Optional[bool] = Field(default=True, description="Актуальность вакансии")
 
     @model_validator(mode="after")
     def validate_salary_range(self) -> Self:
@@ -21,20 +20,13 @@ class JobCreateSchema(BaseModel):
                 raise ValueError("з/п_от не может быть больше з/п_до")
         return self
 
-    @model_validator(mode="after")
-    def validate_created_at(self) -> Self:
-        if self.created_at is not None and self.created_at > datetime.utcnow():
-            raise ValueError("created_at cannot be in the future")
-        return self
-
 
 class JobUpdateSchema(BaseModel):
-    title: str = None
-    description: str = None
-    salary_from: Optional[Decimal] = None
-    salary_to: Optional[Decimal] = None
-    is_active: Optional[bool] = None
-    created_at: Optional[datetime] = None
+    title: str = Field(None, description="Название вакансии")
+    description: str = Field(None, description="Описание вакансии")
+    salary_from: Optional[Decimal] = Field(None, description="Минимальная зарплата")
+    salary_to: Optional[Decimal] = Field(None, description="Максимальная зарплата")
+    is_active: Optional[bool] = Field(None, description="Актуальность вакансии")
 
     @model_validator(mode="after")
     def validate_salary_range(self) -> Self:
@@ -45,11 +37,13 @@ class JobUpdateSchema(BaseModel):
 
 
 class JobSchema(BaseModel):
-    id: int
-    user_id: int
-    title: str
-    description: str
-    salary_from: Decimal
-    salary_to: Decimal
-    is_active: bool
-    created_at: datetime
+    id: int = Field(..., description="Идентификатор вакансии")
+    user_id: int = Field(..., description="Идентификатор пользователя")
+    title: str = Field(..., description="Название вакансии")
+    description: str = Field(..., description="Описание вакансии")
+    salary_from: Optional[Decimal] = Field(None, description="Минимальная зарплата")
+    salary_to: Optional[Decimal] = Field(None, description="Максимальная зарплата")
+    is_active: bool = Field(default=True, description="Актуальность вакансии")
+
+    class Config:
+        from_attributes = True
