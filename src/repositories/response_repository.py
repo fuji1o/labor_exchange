9dd58b557be16b549e55ddce81d4b1af89c9cc5a
fuji1o <fuji.o@yandex.ru>
2025-06-03 00:14:45 +0500

    fix(response_repository):исправлены недочеты после тестов

--- src/repositories/response_repository.py
+++ src/repositories/response_repository.py
@@ -5,9 +5,10 @@ from sqlalchemy import delete, select
 from sqlalchemy.orm import Session, selectinload
 
 from interfaces import IRepositoryAsync
+from models import Job as JobModel
 from models import Response as ResponseModel
+from models import User as UserModel
 from storage.sqlalchemy.tables import Response
-from tools.converter import to_model
 from tools.updater import update_model
 from web.schemas.response import ResponseCreateSchema, ResponseUpdateSchema
 
@@ -27,7 +28,7 @@ class ResponseRepository(IRepositoryAsync):
             session.add(response)
             await session.commit()
             await session.refresh(response)
-        return to_model(response, ResponseModel)
+        return self.__to_response_model(response_from_db=response, include_relations=False)
 
     async def retrieve(self, include_relations: bool = False, **kwargs) -> ResponseModel:
         async with self.session() as session:
@@ -38,7 +39,10 @@ class ResponseRepository(IRepositoryAsync):
             res = await session.execute(query)
             response_from_db = res.scalars().first()
 
-        return to_model(response_from_db, ResponseModel) if response_from_db else None
+        response_model = self.__to_response_model(
+            response_from_db=response_from_db, include_relations=include_relations
+        )
+        return response_model
 
     async def retrieve_many(
         self, limit: int = 100, skip: int = 0, include_relations: bool = False
@@ -53,12 +57,14 @@ class ResponseRepository(IRepositoryAsync):
             res = await session.execute(query)
             responses_from_db = res.scalars().all()
 
-        responses_model = []
+        response_models = []
         for response in responses_from_db:
-            model = to_model(response, ResponseModel)
-            responses_model.append(model)
+            model = self.__to_response_model(
+                response_from_db=response, include_relations=include_relations
+            )
+            response_models.append(model)
 
-        return responses_model
+        return response_models
 
     async def update(self, id: int, response_update_dto: ResponseUpdateSchema) -> ResponseModel:
         async with self.session() as session:
@@ -75,7 +81,7 @@ class ResponseRepository(IRepositoryAsync):
             await session.commit()
             await session.refresh(response_from_db)
 
-        return to_model(response_from_db, ResponseModel)
+        return self.__to_response_model(response_from_db, include_relations=False)
 
     async def delete(self, id: int):
         async with self.session() as session:
@@ -85,5 +91,45 @@ class ResponseRepository(IRepositoryAsync):
 
             if res.rowcount == 0:
                 raise ValueError("Отклик не найден")
+
+        return None
+
+    @staticmethod
+    def __to_response_model(
+        response_from_db: Response, include_relations: bool = False
+    ) -> ResponseModel:
+        user_model = None
+        job_model = None
+
+        if response_from_db:
+            if include_relations:
+                if response_from_db.user:
+                    user_model = UserModel(
+                        id=response_from_db.user.id,
+                        name=response_from_db.user.name,
+                        email=response_from_db.user.email,
+                        is_company=response_from_db.user.user.is_company,
+                    )
+                if response_from_db.user.job:
+                    job_model = JobModel(
+                        id=response_from_db.job.id,
+                        user_id=response_from_db.job.user_id,
+                        title=response_from_db.job.title,
+                        description=response_from_db.job.description,
+                        salary_from=response_from_db.job.salary_from,
+                        salary_to=response_from_db.job.salary_to,
+                        is_active=response_from_db.job.is_active,
+                        created_at=response_from_db.job.created_at,
+                    )
+
+            response_model = ResponseModel(
+                id=response_from_db.id,
+                job_id=response_from_db.job_id,
+                user_id=response_from_db.user_id,
+                message=response_from_db.message,
+                user=user_model,
+                job=job_model,
+            )
+            return response_model
 
         return None
