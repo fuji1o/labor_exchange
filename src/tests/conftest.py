c2f6b0d07654944d9ec98fd20a69e88ceb092aad
fuji1o <fuji.o@yandex.ru>
2025-06-03 00:22:50 +0500

    test(conftest): добавлены фикстуры

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,27 +4,70 @@ from contextlib import asynccontextmanager
 from pathlib import Path
 from unittest.mock import MagicMock
 
-import pytest
 import pytest_asyncio
-from fastapi.testclient import TestClient
+from dependency_injector import providers
+from httpx import AsyncClient
 from sqlalchemy import inspect
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from config import DBSettings
 from main import app
-from repositories import UserRepository
+from repositories.job_repository import JobRepository
+from repositories.response_repository import ResponseRepository
+from repositories.user_repository import UserRepository
+from tools.fixtures.jobs import JobFactory
 from tools.fixtures.users import UserFactory
+from tools.security import create_access_token
+from web.schemas import TokenSchema
 
 env_file_name = ".env." + os.environ.get("STAGE", "test")
 env_file_path = Path(__file__).parent.parent.resolve() / env_file_name
 settings = DBSettings(_env_file=env_file_path)
 
 
-@pytest.fixture()
-def client_app():
-    client = TestClient(app)
-    return client
+@pytest_asyncio.fixture()
+async def current_user(sa_session: AsyncSession):
+    new_user = UserFactory.build()
+
+    async with sa_session() as session:
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+
+    return new_user
+
+
+@pytest_asyncio.fixture()
+async def access_token(current_user):
+    token = TokenSchema(
+        access_token=create_access_token({"sub": current_user.email}), token_type="Bearer"
+    )
+    return token
+
+
+@pytest_asyncio.fixture()
+async def app_with_di():
+    yield app
+
+
+@pytest_asyncio.fixture()
+async def client_with_fake_db(app_with_di, access_token, sa_session):
+    app_with_di.container.job_repository.override(
+        providers.Factory(JobRepository, session=sa_session)
+    )
+
+    app_with_di.container.user_repository.override(
+        providers.Factory(UserRepository, session=sa_session)
+    )
+
+    app_with_di.container.response_repository.override(
+        providers.Factory(ResponseRepository, session=sa_session)
+    )
+
+    async with AsyncClient(app=app_with_di, base_url="http://test") as client:
+        client.headers["Authorization"] = f"Bearer {access_token.access_token}"
+        yield client
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -54,9 +97,6 @@ async def sa_session():
     async def db():
         yield session
 
-    session.commit = MagicMock(side_effect=session.flush)
-    session.delete = MagicMock(side_effect=mock_delete)
-
     try:
         yield db
     finally:
@@ -72,7 +112,13 @@ async def user_repository(sa_session):
     yield repository
 
 
-# регистрация фабрик
+@pytest_asyncio.fixture(scope="function")
+async def job_repository(sa_session):
+    repository = JobRepository(session=sa_session)
+    yield repository
+
+
 @pytest_asyncio.fixture(scope="function", autouse=True)
 def setup_factories(sa_session: AsyncSession) -> None:
-    UserFactory.session = sa_session
+    UserFactory._meta.sqlalchemy_session = sa_session
+    JobFactory._meta.sqlalchemy_session = sa_session
