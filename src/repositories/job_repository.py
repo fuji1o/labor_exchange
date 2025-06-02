a1f8beca997948412a4ba80610d662ed7860292a
fuji1o <fuji.o@yandex.ru>
2025-06-03 00:13:53 +0500

    fix(job_repository):исправлены недочеты после тестов

--- src/repositories/job_repository.py
+++ src/repositories/job_repository.py
@@ -1,4 +1,5 @@
 from contextlib import AbstractContextManager
+from datetime import datetime
 from typing import Callable
 
 from sqlalchemy import delete, select
@@ -6,8 +7,9 @@ from sqlalchemy.orm import Session, selectinload
 
 from interfaces import IRepositoryAsync
 from models import Job as JobModel
+from repositories.response_repository import ResponseModel
+from repositories.user_repository import UserModel
 from storage.sqlalchemy.tables import Job
-from tools.converter import to_model
 from tools.updater import update_model
 from web.schemas.job import JobCreateSchema, JobUpdateSchema
 
@@ -25,14 +27,14 @@ class JobRepository(IRepositoryAsync):
                 salary_from=job_create_dto.salary_from,
                 salary_to=job_create_dto.salary_to,
                 is_active=job_create_dto.is_active,
-                created_at=job_create_dto.created_at,
+                created_at=datetime.utcnow(),
             )
 
             session.add(job)
             await session.commit()
             await session.refresh(job)
 
-        return to_model(job, JobModel)
+        return self.__to_job_model(job_from_db=job, include_relations=False)
 
     async def retrieve(self, include_relations: bool = False, **kwargs) -> JobModel:
         async with self.session() as session:
@@ -42,8 +44,10 @@ class JobRepository(IRepositoryAsync):
 
             res = await session.execute(query)
             job_from_db = res.scalars().first()
-
-        return to_model(job_from_db, JobModel) if job_from_db else None
+        job_model = self.__to_job_model(
+            job_from_db=job_from_db, include_relations=include_relations
+        )
+        return job_model
 
     async def retrieve_many(
         self, limit: int = 100, skip: int = 0, include_relations: bool = False
@@ -56,7 +60,12 @@ class JobRepository(IRepositoryAsync):
             res = await session.execute(query)
             jobs_from_db = res.scalars().all()
 
-        return [to_model(job, JobModel) for job in jobs_from_db]
+        job_models = []
+        for job in jobs_from_db:
+            model = self.__to_job_model(job_from_db=job, include_relations=include_relations)
+            job_models.append(model)
+
+        return job_models
 
     async def update(self, id: int, job_update_dto: JobUpdateSchema) -> JobModel:
         async with self.session() as session:
@@ -74,7 +83,8 @@ class JobRepository(IRepositoryAsync):
             await session.commit()
             await session.refresh(job_from_db)
 
-        return to_model(job_from_db, JobModel)
+        new_job = self.__to_job_model(job_from_db, include_relations=False)
+        return new_job
 
     async def delete(self, id: int):
         async with self.session() as session:
@@ -84,5 +94,42 @@ class JobRepository(IRepositoryAsync):
 
             if res.rowcount == 0:
                 raise ValueError("Вакансия не найдена")
+
+        return None
+
+    @staticmethod
+    def __to_job_model(job_from_db: Job, include_relations: bool = False) -> JobModel:
+        job_model = None
+
+        if job_from_db:
+            if include_relations:
+                if job_from_db.user:
+                    job_user = UserModel(
+                        id=job_from_db.user.id,
+                        name=job_from_db.user.name,
+                        email=job_from_db.user.email,
+                        is_company=job_from_db.user.is_company,
+                    )
+                if job_from_db.responses:
+                    job_responses = [
+                        ResponseModel(
+                            id=response.id,
+                            job_id=response.job_id,
+                            user_id=response.user_id,
+                            message=response.message,
+                        )
+                        for response in job_from_db.responses
+                    ]
+            job_model = JobModel(
+                id=job_from_db.id,
+                user_id=job_from_db.user_id,
+                title=job_from_db.title,
+                description=job_from_db.description,
+                salary_from=job_from_db.salary_from,
+                salary_to=job_from_db.salary_to,
+                is_active=job_from_db.is_active,
+                created_at=job_from_db.created_at,
+            )
+            return job_model
 
         return None
